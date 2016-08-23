636401b1aeadee17844337e1b33405224b4d52d8
tttimit <timit.cloud@gmail.com>
2016-08-23 20:41:51 +0800

    commit 2

--- src/EASY/100_Same_Tree.py
+++ src/EASY/100_Same_Tree.py
@@ -6,3 +6,22 @@
 ## and the nodes have the same value.
 ##
 ## Subscribe to see which companies asked this question
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if q and p:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        elif q is None and p is None: # can use 'p is q' to judge if p and q are the same object
+            return True
+        else:
+            return False
+
+# simple solution on leetcode
+def isSameTree_(self, p, q):
+    if q and q:
+        return p.val == q.val and self.isSameTree_(p.left, q.left) and self.isSameTree_(p.right, q.right)
+    return q is p # object identify
